726c24643ed83b4a1b1b351f65f93a389a7fd72b
Anna <a1n1y1a@gmail.com>
2022-11-24 02:42:18 +0300

    Example_3

---
+++ 1_lesson/1_2/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+if(num == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if(num == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if(num == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if(num == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if(num == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if(num == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if(num == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else 
+{
+    Console.WriteLine("Error");
+}
