a4b7a1ebb5264c546203a8156c717dec59e5cb80
Anna <a1n1y1a@gmail.com>
2022-11-24 02:25:52 +0300

    Example_2

---
+++ 1_lesson/1_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите число 1");
+int num1=int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число 2");
+int num2=int.Parse(Console.ReadLine());
+
+if(num2 == num1*num1)
+{
+    Console.WriteLine("Да");
+}
+
+else
+Console.WriteLine("Нет");
+
