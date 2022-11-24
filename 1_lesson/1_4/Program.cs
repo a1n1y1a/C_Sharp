f53421d8f19f7907548892b60888f27358170b1c
Anna <a1n1y1a@gmail.com>
2022-11-24 03:30:51 +0300

    Example_5

---
+++ 1_lesson/1_4/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число:");
+var s = Console.ReadLine();
+int N = s == null ? 0 : int.Parse(s);
+Console.WriteLine($"{Math.Abs(N)}");
+
