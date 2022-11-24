842f9b192f1f129eed11231925b0eea917ffbb38
Anna <a1n1y1a@gmail.com>
2022-11-24 03:09:25 +0300

    Example_4

---
+++ 1_lesson/1_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите число");
+string num = Console.ReadLine();
+int N = int.Parse(num);
+int Nen = -N;
+
+while (Nen<=N)
+{
+    Console.Write($"{Nen}");
+    Nen +=1;
+}
+
