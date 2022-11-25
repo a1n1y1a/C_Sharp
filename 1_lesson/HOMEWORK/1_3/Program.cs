b06d73ab36501ca29426f918a888a9b708a6ad4d
Anna <a1n1y1a@gmail.com>
2022-11-25 15:20:41 +0300

    Задача_4

--- 1_lesson/HOMEWORK/1_3/Program.cs
+++ 1_lesson/HOMEWORK/1_3/Program.cs
@@ -1,12 +1,13 @@
 ﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-Console.WriteLine("Введите число");
+Console.WriteLine("Введите число:");
 string num = Console.ReadLine();
 int N = int.Parse(num);
-int 
-
-while (Nen<=N)
+int i = 0 ;
+while (i<N-1)
 {
-    Console.Write($"{Nen}");
-    Nen +=1;
+    i+=2;
+    Console.Write(i);
 }
+
+
 
