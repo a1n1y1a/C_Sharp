23e5ed48a235ea299d94f8475b2876f0ac2ffaef
Anna <a1n1y1a@gmail.com>
2022-11-30 08:08:55 +0300

    Zadacha 2_0

---
+++ 2_lesson/2_0/Program.cs
@@ -0,0 +1,16 @@
+﻿// вывод случайное число из отрезка 10-99 и показывает наибольшую цифру числа
+
+int MaxNumber(int num)
+{
+    Console.WriteLine(num);
+
+    if (num % 10 > num / 10)
+        return num % 10;
+
+    else 
+        return num / 10;
+}
+
+ Console.WriteLine(MaxNumber (new Random().Next (10,100)));
+
+// 123456 % 10=6 123456/10=12345
