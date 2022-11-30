ada18e315c98e5bb032537c4a4e5d99094ead27b
Anna <a1n1y1a@gmail.com>
2022-11-30 09:36:55 +0300

    Zadacha4

---
+++ 2_lesson/2_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Void - console.writeline а не ритерн . консоль райтлайн выводит значение на экран
+// string - return string пишем,когда возвращаем ТЕКСТ
+//int - return int пишем,когда возвращаем целое число
+//ритерн нужен чтобы возвращать значения для дальнейших манипуляций
+// на вход число кратно ли оно одновременно 7 и 23
+
+string Div(int num)
+{
+    if(num % 7 == 0 && num % 23 == 0)
+        return "кратно";
+
+    else 
+        return "некратно";
+}
+
+Console.WriteLine("enter number");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(Div(number));
