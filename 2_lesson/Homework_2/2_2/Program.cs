a9911403c2a0c66e3eaec3f1626150c6d519eef9
Anna <a1n1y1a@gmail.com>
2022-11-30 23:48:50 +0300

    Zadacha2_3HW

---
+++ 2_lesson/Homework_2/2_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// вывод третьей цифры заданного числа или сообщает,что ее нет
+
+string T(int num)
+
+{ 
+    if(num>=999)
+
+    return "jfjfj";
+
+    else
+    return "lfkf"
+}
+
+ Console.WriteLine(T(new Random().Next ()));
+Console.WriteLine(T(number));
+
+
+
+//{
+   // if(num >= 999)
+   //     return num % 10;
+
+   // else 
+   //     return "no such figure";
+//}
+
+
+//Console.WriteLine(T(new Random().Next ()));
+//Console.WriteLine(T(number));
---
+++ 2_lesson/Homework_2/2_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// вход цифра, обозначающая день недели,и проверяет является ли этот день выходным
+
+string Week(int num)
+{
+    if(num <=5 && num>0)
+        return "no";
+
+    if(num>5 && num<7)
+        return "yes";
+    else 
+        return "error";
+}
+
+Console.WriteLine("number");
+int first = int.Parse(Console.ReadLine());
+Console.WriteLine(Week(first));
