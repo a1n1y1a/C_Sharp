98272137404b754eca8c5733d082648a972723ac
Anna <a1n1y1a@gmail.com>
2022-11-30 08:27:00 +0300

    Zadacha_2

---
+++ 2_lesson/2_1/Program.cs
@@ -0,0 +1,8 @@
+﻿// выводит случайное трехзначное число и выводит последнюю цифру этого числа
+int LastNumber(int num)
+{
+    Console.WriteLine(num);
+    return num % 10;
+}
+
+ Console.WriteLine(LastNumber (new Random().Next (100,1000)));
