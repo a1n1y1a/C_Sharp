405e85156cc30fd50567a10531129ec0b2495899
Anna <a1n1y1a@gmail.com>
2022-11-30 22:49:39 +0300

    Zadacha2_0HW

---
+++ 2_lesson/Homework_2/2_0/Program.cs
@@ -0,0 +1,10 @@
+﻿// Принимает на вход трехзначноетчисло,а на выходе показывает вторую цифру этого числа
+
+int SecondNumber(int num)
+{
+    Console.WriteLine(num);
+    return num / 10 % 10;
+}
+
+ Console.WriteLine(SecondNumber (new Random().Next (100,1000)));
+
