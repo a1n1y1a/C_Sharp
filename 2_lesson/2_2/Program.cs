6e3c1c0de423d407c7b57254f3c081903f8adcc0
Anna <a1n1y1a@gmail.com>
2022-11-30 08:53:25 +0300

    Zadacha_3

---
+++ 2_lesson/2_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// заходит два числа кратно ли одно второму если не кратно то выводит остаток
+
+
+string crat(int num, int num_2)
+{
+    if(num % num_2 == 0)
+        return "кратно";
+
+    else 
+        return $"некратно, остаток= {num % num_2}";
+}
+
+Console.WriteLine("1 chislo");
+int first = int.Parse(Console.ReadLine());
+Console.WriteLine("2 chislo");
+int second = int.Parse(Console.ReadLine());
+Console.WriteLine(crat(first,second));
