d4651063bb53c5f2d38c3e850425dd247e5ec905
Anna <a1n1y1a@gmail.com>
2022-11-30 23:03:30 +0300

    Zadacha2_1HW

---
+++ 2_lesson/Homework_2/2_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// выводит случайное трехзначное число и удаляет вторую цифру этого числа
+
+int D(int num)
+{
+    Console.WriteLine(num);
+    return num / 100*10 + num % 10;
+}
+
+ Console.WriteLine(D(new Random().Next (100,1000)));
+
