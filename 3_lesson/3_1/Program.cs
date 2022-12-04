462874cb163350f7c0712da14963173ea33a204f
Anna <a1n1y1a@gmail.com>
2022-12-04 23:31:39 +0300

    Zadacha_3_1

---
+++ 3_lesson/3_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// по заданному номеру чевтерти выводит диапозон возможных кооринат точек в жтой четверти икс и игрик
+
+void Quarters(string a)
+{
+    if (a == "I")
+    Console.WriteLine ("x>0 and y>0");
+
+     else if (a == "II")
+    Console.WriteLine ("x<0 and y>0"); 
+
+    else if (a == "III")
+    Console.WriteLine ("x<0 and y<0"); 
+
+    else if (a == "IV")
+    Console.WriteLine ("x>0 and y<0"); 
+
+    else 
+    Console.WriteLine("error");
+}
+Quarters(Console.ReadLine());
