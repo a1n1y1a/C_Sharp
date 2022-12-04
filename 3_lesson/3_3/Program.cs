50a976cb56837cbf35acac04f8350fc0b4cdc16e
Anna <a1n1y1a@gmail.com>
2022-12-05 01:06:31 +0300

    Zadacha_3_3

---
+++ 3_lesson/3_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// на вход принимает число N и выдает таблицу квадратов чисел от 1 до N
+
+void Chet (int N)
+{
+    int i = 0;
+    double pere ;
+    while (i < N)
+    {
+        i+=1;
+        if (i == (N))
+        {
+            pere=Math.Pow(i,2);
+            Console.Write($"{pere}");
+        }
+       else
+       {
+         pere=Math.Pow(i,2);
+            Console.Write($"{pere},");
+       }
+    }
+}
+Console.WriteLine ("введите ч");
+int chislo = int.Parse (Console.ReadLine());
+Chet(chislo);
