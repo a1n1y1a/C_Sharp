74a3930c0687dc408899f0f0f09ac512f7e2e99f
Anna <a1n1y1a@gmail.com>
2022-12-05 00:36:12 +0300

    Zadacha_3_2

---
+++ 3_lesson/3_0/3_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// принимает координаты двух точек и находит расстояние между ними в 2D пространстве
+
+int ReadNumber(string message)
+{
+    Console.Write (message);
+    var s = Console.ReadLine();
+    int a = s == null ? 0 : int.Parse (s);
+    return a;
+}
+double Distance(int ax, int ay, int bx, int by)
+{
+    double result;
+    result = Math.Sqrt(Math.Pow(bx-ax,2)+Math.Pow(by-ay,2));
+    return result;
+}
+int ax = ReadNumber("Введите ax:");
+int ay = ReadNumber("Введите ay:");
+int bx = ReadNumber("Введите bx:");
+int by = ReadNumber("Введите by:");
+
+Console.WriteLine (Math.Round(Distance(ax,ay,bx,by),2));
