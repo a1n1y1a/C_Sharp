e2e5ecdddf752cfec9db798d7e54a0bb1df814c5
Anna <a1n1y1a@gmail.com>
2022-12-05 14:32:08 +0300

    Zadacha3_3HW

---
+++ 3_lesson/Homework_3/3_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая ппринимает на вход число N и выводит таблицу кубов чисел от 1 до N
+
+void Kub (int N)
+{
+    int i = 0;
+    double pere ;
+    while (i < N)
+    {
+        i+=1;
+        if (i == (N))
+        {
+            pere=Math.Pow(i,3);
+            Console.Write($"{pere}");
+        }
+       else
+       {
+         pere=Math.Pow(i,3);
+            Console.Write($"{pere},");
+       }
+    }
+}
+Console.WriteLine ("введите число");
+int chislo = int.Parse (Console.ReadLine());
+Kub(chislo);
