2937d8bfc59974519e21aecbe0b2440a52923c44
Anna <a1n1y1a@gmail.com>
2022-12-07 01:10:15 +0300

    Zadacha4_3

---
+++ 4_lesson/4_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// 3. Напишите программу, которая выводит массив из 8 элементов,
+// заполненный нулями и единицами в случайном порядке.
+
+void Massiv(int n)
+{
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+array[i] = new Random().Next(2);
+Console.Write($"{array[i]}, ");
+}
+}
+
+Console.WriteLine("Введите число:");
+Massiv(int.Parse(Console.ReadLine()));
+// ТК у нас в теле есть write то в конце мы не пишем write
