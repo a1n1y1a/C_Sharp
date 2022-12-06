f3ce290b033f79e47dd54d5a407b0fcce392a0e1
Anna <a1n1y1a@gmail.com>
2022-12-07 00:57:59 +0300

    Zadacha4_2

---
+++ 4_lesson/4_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// принимает на вход число N и выдает произведение чисел от 1 до N
+
+int Proiz(int num)
+{
+    int all_sum = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        all_sum = all_sum * i;
+    }
+    return all_sum;
+}
+
+Console.WriteLine(Proiz(int.Parse(Console.ReadLine())));
