f3f266aec6de803972b3e6662e0cb4405dafaf7f
Anna <a1n1y1a@gmail.com>
2022-12-07 19:20:58 +0300

    Zadacha4_1HW

---
+++ 4_lesson/HOMEWORK/4_3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+void Mas(int n)
+{
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+array[i] = Console.WriteLine ("Введите элементы массива") ;
+Console.Write($"{array[i]}, ");
+}
+}
+
+Console.WriteLine("Введите элементы массива:");
+M(int.Parse(Console.ReadLine()));
