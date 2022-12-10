23cbbdf1484747a1834667bbf53a3090cd097fec
Anna <a1n1y1a@gmail.com>
2022-12-11 01:45:53 +0300

    Zadacha5_2

---
+++ 5_lesson/5_2/Program.cs
@@ -0,0 +1,33 @@
+﻿// // 2. Задайте массив. Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве.
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] Mass(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(-100, 100);
+    return arr;
+}
+
+string Poisk(int[] array, int num)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(num == array[i])
+        return "Yes";
+    }
+    return "No";
+}
+int[] arr_1 = Mass(int.Parse(Console.ReadLine()));
+Print(arr_1);
+Console.WriteLine(Poisk (arr_1,int.Parse(Console.ReadLine())));
