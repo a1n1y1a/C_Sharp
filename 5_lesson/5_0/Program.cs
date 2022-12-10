bbc132aaa2286830c493c1deae3ed4d8064bc805
Anna <a1n1y1a@gmail.com>
2022-12-11 01:02:27 +0300

    Zadacha5_0

--- 5_lesson/5_0/Program.cs
+++ 5_lesson/5_0/Program.cs
@@ -1,1 +1,59 @@
-﻿// 
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9].
+// Найдите сумму отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29,
+// сумма отрицательных равна -20.
+
+
+void Massiv(int n)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next();
+        Console.Write($"{array[i]}, ");
+    }
+}
+Console.WriteLine("Введите число:");
+Massiv(int.Parse(Console.ReadLine()));
+
+//
+
+void Print(int[] arr)
+{
+    int size = arr.Lenght;
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]}");
+    Console.WriteLine();
+
+}
+
+int[] MassNums(int size,int from,int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new.Random().Next(from, to);
+    return arr;
+}
+void SumPosNeg(int[]arr)
+{
+    int pos,neg;
+    pos=neg=0;
+
+    for (int i=0, i<arr.Lenght;i++)
+    {
+        if (arr[i] >=0)
+        pos += arr[i];
+        else
+        neg +=arr[i];
+    }
+
+    Console.WriteLine($"Positive: {pos}, negative: {neg}");
+
+}
+
+int[] arr_1 = Eightmass(int.Parse(Console.ReadLine()),
+                       (int.Parse(Console.ReadLine()),
+                       (int.Parse(Console.ReadLine()));
+Print(arr_1);
+SumPosNeg(arr_1);
