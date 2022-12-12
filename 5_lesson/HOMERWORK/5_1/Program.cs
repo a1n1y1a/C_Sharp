32027d60b7fc069bbc9474c9a894a2fe6678b882
Anna <a1n1y1a@gmail.com>
2022-12-12 14:08:47 +0300

    Zadacha5_1HW

---
+++ 5_lesson/HOMERWORK/5_1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] Mass(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(100, 1000);
+    return arr;
+}
+
+int SumChet(int[]array)
+{
+     int summa = 0;
+    for (int i = 0; i < array.Length; i++)
+   
+    {
+        if (array[i] % 2 == 0)
+            summa = summa + 1;
+    }
+    return summa;
+}
+int[] arr_1 = Mass(int.Parse(Console.ReadLine()));
+Print(arr_1);
+Console.Write(SumChet(arr_1));
