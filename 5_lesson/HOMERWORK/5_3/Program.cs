d09a54f9e26879949a23383086f992336a246f80
Anna <a1n1y1a@gmail.com>
2022-12-12 16:02:33 +0300

    Zadacha5_3HW

--- 5_lesson/HOMERWORK/5_3/Program.cs
+++ 5_lesson/HOMERWORK/5_3/Program.cs
@@ -1,3 +1,46 @@
 ﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 
 //[3 7 22 2 78] -> 76
+
+void Print(double[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+double[] Mass(int size, int begin, int end)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+        arr[i] = Math.Round(rnd.NextDouble() * (end + begin) - begin, 1);
+    return arr;
+}
+
+void Raznica(double[] array)
+{
+    double max = array[0];
+    double min = array[1];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= max)
+            max = array[i];
+
+
+        else if (array[i] <= min)
+            min = array[i];
+
+    }
+    Console.Write($"Максимальное:{max}, Минимальное:{min}. ");
+    Console.Write($"Разница : {max}-{min}={Math.Round(max-min,1)}");
+}
+double[] arr_1 = Mass(int.Parse(Console.ReadLine()),
+                      int.Parse(Console.ReadLine()),
+                      int.Parse(Console.ReadLine()));
+Print(arr_1);
+Raznica(arr_1);
+
