e332f8607a7420666d9f8d8c37eee30c3e3b055c
Anna <a1n1y1a@gmail.com>
2022-12-12 14:18:40 +0300

    Zadacha5_2HW

--- 5_lesson/HOMERWORK/5_2/Program.cs
+++ 5_lesson/HOMERWORK/5_2/Program.cs
@@ -6,3 +6,35 @@
 //[-4, -6, 4, 6] -> 0
 
 
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] Mass(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(10,100);
+    return arr;
+}
+
+int Sum2(int[] array)
+{
+    int summa = 0;
+    for (int i = 0; i < array.Length; i++)
+
+    {
+        if (i % 2 == 0)
+            summa = summa + array[i];
+    }
+    return summa;
+}
+int[] arr_1 = Mass(int.Parse(Console.ReadLine()));
+Print(arr_1);
+Console.Write(Sum2(arr_1));
