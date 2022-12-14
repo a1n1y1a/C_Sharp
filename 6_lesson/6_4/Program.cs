1e9341c3067d1eff197d13c1de437861e782af71
Anna <a1n1y1a@gmail.com>
2022-12-15 01:04:14 +0300

    Zadacha6_4

--- 6_lesson/6_4/Program.cs
+++ 6_lesson/6_4/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 4. Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного копирования.
+
+void Massiv(int[,] arr)
+{
+    int stolb = arr.GetLength(1);
+    int raw = arr.GetLength(0);
+
+    for (int i = 0; i < raw; i++)
+    {
+        for (int j = 0; j < stolb; j++)
+            Console.Write($"{arr[i, j]} ");
+        Console.WriteLine();
+    }
+}
+int[,] Print(int raw, int stolb)
+{
+    int[,] arr = new int[raw, stolb];
+
+    for (int i = 0; i < raw; i++)
+        for (int j = 0; j < stolb; j++)
+            arr[i, j] = new Random().Next(100, 1000);
+    return arr;
+}
