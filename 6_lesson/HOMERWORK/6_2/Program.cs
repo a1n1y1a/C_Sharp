d7c9afa0c12bfd7d7836f3ae2f1ae3fbf5671c05
Anna <a1n1y1a@gmail.com>
2022-12-17 00:18:44 +0300

    Zadacha6_2

--- 6_lesson/HOMERWORK/6_2/Program.cs
+++ 6_lesson/HOMERWORK/6_2/Program.cs
@@ -4,7 +4,25 @@
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+void Tochka(double b1, double k1, double b2, double k2)
+{
+    double razn = k1 - k2;
+    if (razn != 0)
+    {
+        double x = (b2 - b1)/razn;
+        double y = k1 * x + b1;
+        Console.WriteLine($"Точка пересечения: ({x};{y})");
+    }
 
 
-string Tochka()
-int 
+    else
+       {
+         Console.WriteLine("Точки пересечния нет");
+       }
+}
+
+Tochka(int.Parse(Console.ReadLine()),
+      int.Parse(Console.ReadLine()),
+      int.Parse(Console.ReadLine()),
+      int.Parse(Console.ReadLine()));
+
