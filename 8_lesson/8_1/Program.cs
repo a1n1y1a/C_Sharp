0daedf6e651ec5e9a633f56a2c884c78c3e1b698
Anna <a1n1y1a@gmail.com>
2022-12-23 00:02:44 +0300

    Zadacha8_1

--- 8_lesson/8_1/Program.cs
+++ 8_lesson/8_1/Program.cs
@@ -3,33 +3,36 @@
 
 void Print(int[,] arr)
 {
-int row_size = arr.GetLength(0);
-int column_size = arr.GetLength(1);
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
 
-for (int i = 0; i < row_size; i++)
-{
-for (int j = 0; j < column_size; j++)
-Console.Write($" {arr[i, j],4} ");
-Console.WriteLine();
-}
-Console.WriteLine();
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],4} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
 }
 
 int[,] MassNums(int row, int column, int from, int to)
 {
-int[,] arr = new int[row, column];
+    int[,] arr = new int[row, column];
 
-for (int i = 0; i < row; i++)
-for (int j = 0; j < column; j++)
-arr[i, j] = new Random().Next(from, to);
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
 
-return arr;
+    return arr;
 }
 
 void Change(int[,] arr)
 {
-for (int i=0; i<row ;i++)
-for (int j =0; j<column; j++)
+    
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    for (int i = 0; i < column; i++)
+        (arr[0,i],arr[row-1,i])=(arr[row-1,i],arr[0,i]);
 
 }
 
@@ -39,7 +42,9 @@ Console.Write("Enter the number of columns: ");
 int column = int.Parse(Console.ReadLine());
 
 int[,] arr_1 = MassNums(row, column,
-int.Parse(Console.ReadLine()),
-int.Parse(Console.ReadLine()));
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+Print (arr_1);                       
+Change(arr_1);
 Print(arr_1);
-Console.WriteLine(Summ(arr_1));
+
--- 8_lesson/8_3/Program.cs
+++ 8_lesson/8_3/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],4} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+
+    return arr;
+}
