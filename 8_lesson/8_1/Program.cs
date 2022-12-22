71711bd869309da9c843322cf904eacc66b77793
Anna <a1n1y1a@gmail.com>
2022-12-22 23:40:26 +0300

    Zadacha8_2

---
+++ 8_lesson/8_1/Program.cs
@@ -0,0 +1,45 @@
+﻿// // 1. Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку массива.
+
+void Print(int[,] arr)
+{
+int row_size = arr.GetLength(0);
+int column_size = arr.GetLength(1);
+
+for (int i = 0; i < row_size; i++)
+{
+for (int j = 0; j < column_size; j++)
+Console.Write($" {arr[i, j],4} ");
+Console.WriteLine();
+}
+Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+int[,] arr = new int[row, column];
+
+for (int i = 0; i < row; i++)
+for (int j = 0; j < column; j++)
+arr[i, j] = new Random().Next(from, to);
+
+return arr;
+}
+
+void Change(int[,] arr)
+{
+for (int i=0; i<row ;i++)
+for (int j =0; j<column; j++)
+
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column,
+int.Parse(Console.ReadLine()),
+int.Parse(Console.ReadLine()));
+Print(arr_1);
+Console.WriteLine(Summ(arr_1));
---
+++ 8_lesson/8_2/Program.cs
@@ -0,0 +1,60 @@
+﻿// 2. Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это невозможно,
+// программа должна вывести сообщение для пользователя.
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],4} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+
+    return arr;
+}
+
+void ChangeUp(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    if (row == column)
+    {
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                (arr[i, j], arr[j, i]) = (arr[j, i], arr[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+    else
+    {
+        Console.WriteLine("Замена невозможна");
+    }
+
+}
+
+int[,] arr1 = MassNums(int.Parse(Console.ReadLine()),
+                    int.Parse(Console.ReadLine()),
+                    int.Parse(Console.ReadLine()),
+                    int.Parse(Console.ReadLine()));
+
+Print(arr1);
+ChangeUp(arr1);
+Console.WriteLine();
+Print(arr1);
