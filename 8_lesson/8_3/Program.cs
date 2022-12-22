3a9fff66e88c697529df9a2aa5c03996e3b6ba8a
Anna <a1n1y1a@gmail.com>
2022-12-23 00:25:31 +0300

    Zadacha8_3

--- 8_lesson/8_3/Program.cs
+++ 8_lesson/8_3/Program.cs
@@ -1,4 +1,6 @@
-﻿// 
+﻿// // 3. Составить частотный словарь элементов двумерного массива.
+// Частотный словарь содержит информацию о том, сколько раз
+// встречается элемент входных данных. Значения элементов массива 0..9
 
 void Print(int[,] arr)
 {
@@ -24,3 +26,33 @@ int[,] MassNums(int row, int column, int from, int to)
 
     return arr;
 }
+
+void PrintMass(int[] arr)
+{
+for (int i = 0; i < arr.Length; i++)
+Console.WriteLine($"{i} meets: {arr[i]}");
+Console.WriteLine();
+}
+
+int[] FrequencyDict(int[,] arr)
+{
+int[] freq = new int[10];
+
+foreach (int item in arr)
+freq[item] += 1;
+
+return freq;
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column,
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+Print (arr_1);     
+
+int[]mass= FrequencyDict(arr_1);
+PrintMass(mass);
