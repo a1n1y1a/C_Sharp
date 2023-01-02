172297a29adb8b1f9c8071ec5f719b3405781fed
Anna <a1n1y1a@gmail.com>
2023-01-02 14:39:00 +0300

    Zadacha 9_2

--- 9_lesson/9_1/Program.cs
+++ 9_lesson/9_1/Program.cs
@@ -1,9 +1,10 @@
-﻿// 
+﻿// Задайте значение N. Напишите прошрамму,которая выведет 
+//все натуральные числа в промежутке от 1 до N при помощи рекурсиию
 
-void Chisla (int a)
+void Chisla(int a)
 {
-    if (a==0) return;
-    Chisla (a-1);
-    Console.Write($"{a}" );
+    if (a == 0) return;
+    Chisla(a - 1);
+    Console.Write($"{a}");
 }
-Chisla (9);
+Chisla(9);
