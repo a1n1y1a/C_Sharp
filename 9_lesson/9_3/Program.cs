0af5261ad3aece64e3a94dea074af760e5df9a97
Anna <a1n1y1a@gmail.com>
2023-01-02 15:15:24 +0300

    Zadacha 9_4

--- 9_lesson/9_3/Program.cs
+++ 9_lesson/9_3/Program.cs
@@ -1,10 +1,10 @@
 ﻿// Напишите программу, которая будет принимать на вход число и 
 //возвращать сумму его цифр. Использовать рекурсию.
 
-int Summ (int a)
+int Summ(int a)
 {
-    if (a==0) return 0;
-    return Summ (a/10)+a%10;
+    if (a == 0) return 0;
+    return Summ(a / 10) + a % 10;
 }
 
 Console.WriteLine(Summ(453));
