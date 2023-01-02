5e6614fa8c91aaed21a1147f60dc759f24dd4fb7
Anna <a1n1y1a@gmail.com>
2023-01-02 15:03:11 +0300

    Zadacha 9_3

--- 9_lesson/9_3/Program.cs
+++ 9_lesson/9_3/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет принимать на вход число и 
+//возвращать сумму его цифр. Использовать рекурсию.
+
+int Summ (int a)
+{
+    if (a==0) return 0;
+    return Summ (a/10)+a%10;
+}
+
+Console.WriteLine(Summ(453));
